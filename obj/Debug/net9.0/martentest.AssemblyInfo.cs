b3eeab8cb3159eb95349de107bfc4b52802039ad
Pascal Senn <pascal@chillicream.com>
2025-04-29 22:15:02 +0200

    add gitingore

--- obj/Debug/net9.0/martentest.AssemblyInfo.cs
+++ obj/Debug/net9.0/martentest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("martentest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb19d50aff787c158d4103e7f3f70e0c71496481")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8baf16458906a659899da0b266f854e6aa13448d")]
 [assembly: System.Reflection.AssemblyProductAttribute("martentest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("martentest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
